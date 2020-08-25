e518f9b317b6b6b9e6968181a00635013954aeee
AsutoshPati <AsutoshPati@users.noreply.github.com>
2020-08-25 09:21:23 +0530

    update

---
+++ js/courses.js
@@ -0,0 +1,136 @@
+//alert("JS loaded");  
+
+function getInformation(){
+    //alert("getInfo called");
+    var course_informations = [
+        {
+            "Name": "DIPLOMA IN TOOL & DIE MAKING (DTDM)", 
+            "Fee": "₹ 19,000 (per semester)", 
+            "Duration": "4 years",
+            "Selection": "Selection will be based on all India level Entrance Examination",
+            "Duration Type": "L", 
+            "Qualification": "X",
+            "Short": "DTDM", 
+            "Image": "images/test.jpg",
+            "Description": `After completion of the course, the Trainees will
+            be able to design & manufacture different types of
+            tools & dies using CAD/CAM software & all kind of
+            conventional & CNC machine.<br><br>
+            Equivalent to Diploma in Mechanical Engg. With
+            TOOL/Mould/Die/FixtureDesigning&Manufacturing.
+            N.B. : Course contains 70% Practical & 30%
+            Theory.`
+        },
+
+        {
+            "Name": "DIPLOMA IN MECHATRONICS (DIM)", 
+            "Fee": "₹ 19,000 (per semester)", 
+            "Duration": "3 years",
+            "Selection": "Selection will be based on all India level Entrance Examination",
+            "Duration Type": "L", 
+            "Qualification": "X",
+            "Short": "DIM", 
+            "Image": "images/test.jpg",
+            "Description": `After completion of the course, the Trainees will
+            be able to work in different types of mechanical as
+            well as electronics machineries and can be able to
+            manufacture different mechatronics components.<br><br>
+            Combined subjects of Mechanical, Electrical &
+            Electronics
+            N.B. : Course contains 70% Practical & 30%
+            Theory.`
+        },
+
+        {
+            "Name": "ITI MACHINIST", 
+            "Fee": "₹ 28,000 (per year)", 
+            "Duration": "2 years",
+            "Selection": "Selection will be based on HSC mark",
+            "Duration Type": "L", 
+            "Qualification": "X",
+            "Short": "ITIM", 
+            "Image": "images/test.jpg",
+            "Description": `As per NCVT Syllabus<br><br>
+            Course location - CTTC, Bhubaneswar`
+        },
+
+        {
+            "Name": "ITI FITTER", 
+            "Fee": "₹ 28,000 (per year)", 
+            "Duration": "2 years",
+            "Selection": "Selection will be based on HSC mark",
+            "Duration Type": "L", 
+            "Qualification": "X",
+            "Short": "ITIF", 
+            "Image": "images/test.jpg",
+            "Description": `As per NCVT Syllabus<br><br>
+            Course location - CTTC, Kalinga nagar, Jajpur`
+        },
+
+        {
+            "Name": "ITI ELECTRICIAN", 
+            "Fee": "₹ 28,000 (per year)", 
+            "Duration": "2 years",
+            "Selection": "Selection will be based on HSC mark",
+            "Duration Type": "L", 
+            "Qualification": "X", 
+            "Short": "ITIE",
+            "Image": "images/test.jpg",
+            "Description": `As per NCVT Syllabus<br><br>
+            Course location - CTTC, Kalinga nagar, Jajpur`
+        },
+
+        {
+            "Name": "CONDENSED CERTIFICATE COURSE IN TOOL & DIE MAKING (CTDM)", 
+            "Fee": "₹ 66,000", 
+            "Duration": "1 year",
+            "Selection": "First come first serve basis",
+            "Duration Type": "L", 
+            "Qualification": "X", 
+            "Short": "CTDM",
+            "Image": "images/test.jpg",
+            "Description": `NSQF Level - 5<br><br>
+            Job Role - Skilled Technician/Tool and Die Maker<br><br>
+            After completion of the course, the trainees will be able
+            to handle all types of machine as well as can
+            manufacture different types of tool & die.<br><br>
+            <b>Semester-I</b> : Production technology, Auto CAD, Part
+            Programming, Master Cam, Press Tool Design, CNC
+            Technology, Engineering Drawing, Material
+            Technology, Metrology, Press Tool Theory.<br><br>
+            <b>Semester-II</b> : Press Tool Design, Mold Manufacturing,
+            Modern Manufacturing Technology, Jigs and Fixture,
+            Mold Theory, Communicative English, Industrial
+            Management, In plant Training & Project Work.
+            `
+        },
+
+        {
+            "Name": "CERTIFICATE COURSE IN CNC TURNING & MILLING (CCTM)", 
+            "Fee": "₹ 66,000", 
+            "Duration": "1 year",
+            "Selection": "First come first serve basis",
+            "Duration Type": "L", 
+            "Qualification": "X", 
+            "Short": "CCTM",
+            "Image": "images/test.jpg",
+            "Description": `NSQF Level - 5<br><br>
+            Job Role - Skilled Technician/Tool and Die Maker<br><br>
+            After completion of the course, the trainees will be able
+            to handle all types of machine as well as can
+            manufacture different types of tool & die.<br><br>
+            <b>Semester-I</b> : Production technology, Auto CAD, Part
+            Programming, Master Cam, Press Tool Design, CNC
+            Technology, Engineering Drawing, Material
+            Technology, Metrology, Press Tool Theory.<br><br>
+            <b>Semester-II</b> : Press Tool Design, Mold Manufacturing,
+            Modern Manufacturing Technology, Jigs and Fixture,
+            Mold Theory, Communicative English, Industrial
+            Management, In plant Training & Project Work.
+            `
+        },
+
+    ];
+
+    return course_informations;
+}
